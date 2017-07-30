b256c35784653e0b177b9f670197d0382fa16189
Brian Empson <brian.empson@gmail.com>
2017-07-30 18:15:02 -0400

    Category Updated

--- js/controllers/categoriesController.js
+++ js/controllers/categoriesController.js
@@ -1,5 +1,10 @@
 (function() {
 
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date+' '+time;
+    
     var as = angular.module('crafts.controllers');
     
     as.controller('CategoriesCtrl', function($scope, $rootScope, $http, $location) {
@@ -10,7 +15,7 @@
             $http.get($rootScope.appUrl + '/api/Categories.json')
 
                     .success(function(data, status, headers, config) {
-                        console.log('SUCCESS!!!  ');
+                        console.log('Get Category Data...');
                         $scope.categories = data.categories;
                     });
         }
@@ -21,12 +26,29 @@
             console.log('call New Category');
             $location.path("/new-category");
         }
-    
+
+        $scope.editCategory = function(index) {
+            console.log('call editCategory');
+            $location.path('/edit-category/' + $scope.categories[index].id);
+        }
+
+        $scope.delCategory = function(index) {
+            console.log('call delCategory');
+            var todel = $scope.categories[index];
+            $http
+                    .delete($rootScope.appUrl + '/api/categories/delete/' + todel.id + '.json')
+                    .success(function(data, status, headers, config) {
+                        load();
+                    }).error(function(data, status, headers, config) {
+            });
+        }
+            
     });
 
     as.controller('NewCategoryCtrl', function($scope, $rootScope, $http, $location) {
 	    
 	    var getdep = function() {
+			console.log('call New Category...');
             $http.get($rootScope.appUrl + '/api/Departments.json')
                     .success(function(data, status, headers, config) {
                         $scope.departments = data.departments;                      
@@ -37,6 +59,7 @@
         $scope.category = {};
 
         $scope.saveCategory = function() {
+			console.log('call Save Category...');
 		    var _data = {};
             _data = $scope.category;
 
@@ -54,4 +77,48 @@
 		}
 	
 	});
+	    
+	as.controller('EditCategoryCtrl', function($scope, $rootScope, $http, $routeParams, $location) {
+
+        var getdep = function() {
+			console.log('Grab Departments...');
+            $http.get($rootScope.appUrl + '/api/Departments.json')
+                    .success(function(data, status, headers, config) {
+                        $scope.departments = data.departments;                      
+                    });
+        }
+        getdep();
+        
+        var load = function() {
+            console.log('Grab Category '+ $routeParams['id'] +' ...');
+            $http.get($rootScope.appUrl + '/api/Categories/view/' + $routeParams['id'] + '.json')
+                    .success(function(data, status, headers, config) {
+                        $scope.category = data.category;
+                        angular.copy($scope.category, $scope.copy);
+                    });
+        }
+
+        load();
+
+        $scope.category = {};
+
+        $scope.updateCategory = function() {
+            console.log('Call Update Category...');
+        
+            var _data = {};
+                delete $scope.category.department.modified;
+                delete $scope.category.department.created;
+                delete $scope.category.created;
+                $scope.category.modified = dateTime;
+                _data = $scope.category;
+                            
+                $http
+                    .put($rootScope.appUrl + '/api/Categories/edit/' + $scope.category.id + '.json', _data)
+                    .success(function(data, status, headers, config) {
+                        $location.path('/categories');
+                    }).error(function(data, status, headers, config) {
+            });
+        }
+    });
+    
 }());    
